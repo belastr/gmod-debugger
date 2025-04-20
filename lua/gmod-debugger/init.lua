b9a0b18f2e4792bc53d21530c89835dce4ebc9e3
Sooth <128935065+Sooth4@users.noreply.github.com>
2025-04-20 10:37:03 +0200

    fix: find derma files to load

--- lua/gmod-debugger/init.lua
+++ lua/gmod-debugger/init.lua
@@ -2,12 +2,12 @@ GMOD_DEBUGGER = {}
 GMOD_DEBUGGER.logs = {}
 
 if CLIENT then
-    for _, f in ipairs(file.Find("lua/gmod-debugger/derma/*.lua", "DATA")) do
+    for _, f in ipairs(file.Find("gmod-debugger/derma/*.lua", "LUA")) do
         include("gmod-debugger/derma/" .. f)
     end
     include("gmod-debugger/menu.lua")
 elseif SERVER then
-    for _, f in ipairs(file.Find("lua/gmod-debugger/derma/*.lua", "DATA")) do
+    for _, f in ipairs(file.Find("gmod-debugger/derma/*.lua", "LUA")) do
         AddCSLuaFile("gmod-debugger/derma/" .. f)
     end
     AddCSLuaFile("gmod-debugger/menu.lua")
