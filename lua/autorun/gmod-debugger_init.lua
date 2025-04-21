e03c95a30300e515f548490e5625702d7a2cfa17
Sooth <128935065+Sooth4@users.noreply.github.com>
2025-04-21 23:26:21 +0200

    fix: load modules based on config after config receival

--- lua/autorun/gmod-debugger_init.lua
+++ lua/autorun/gmod-debugger_init.lua
@@ -4,11 +4,6 @@ if CLIENT && GMOD_DEBUGGER then
     include("gmod-debugger/init.lua")
     hook.Add("InitPostEntity", "gmod-debugger:init", function()
         GMOD_DEBUGGER:RequestConfig()
-        if !GMOD_DEBUGGER.config then return end
-    
-        for _, mod in ipairs(GMOD_DEBUGGER.config.enabledModules) do
-            include("gmod-debugger/modules/" .. mod .. "/init.lua")
-        end
     end)
 elseif SERVER && GMOD_DEBUGGER then
     local col = Color(88, 101, 242)
--- lua/gmod-debugger/menu.lua
+++ lua/gmod-debugger/menu.lua
@@ -10,4 +10,8 @@ end
 net.Receive("gmod-debugger:config", function(len)
     local configData = net.ReadData(len / 8)
     GMOD_DEBUGGER.config = util.JSONToTable(util.Decompress(configData), false, true)
+    
+    for _, mod in ipairs(GMOD_DEBUGGER.config.enabledModules) do
+        include("gmod-debugger/modules/" .. mod .. "/init.lua")
+    end
 end)
