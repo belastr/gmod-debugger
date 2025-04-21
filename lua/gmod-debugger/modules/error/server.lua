2067d21447925ee39a2fb99308cf64a8177008d9
Sooth <128935065+Sooth4@users.noreply.github.com>
2025-04-21 23:29:41 +0200

    refactor: save error logs through new core function

--- lua/gmod-debugger/modules/error/server.lua
+++ lua/gmod-debugger/modules/error/server.lua
@@ -1,9 +1,16 @@
-hook.Add("OnLuaError", "gmod-debugger:error", function(error_msg, _, error_stack, addon)
+hook.Add("OnLuaError", "gmod-debugger:error", function(error_msg, _, error_stack)
     if GMOD_DEBUGGER.config.error.server then
-        if !GMOD_DEBUGGER.logs.error[error_msg] then
-            GMOD_DEBUGGER.logs.error[error_msg] = {stack = GMOD_DEBUGGER.config.error.stack && error_stack || false, count = 1}
+        local d = {stack = GMOD_DEBUGGER.config.error.stack && error_stack || false, count = 1}
+        GMOD_DEBUGGER:SaveLog("error", {msg = error_msg, data = d})
+    end
+end)
+
+hook.Add("gmod-debugger:saveLog", "gmod-debugger:error", function(mod, log)
+    if mod == "error" && GMOD_DEBUGGER.config.error.server then
+        if !GMOD_DEBUGGER.logs.error[log.msg] then
+            GMOD_DEBUGGER.logs.error[log.msg] = log.data
         else
-            GMOD_DEBUGGER.logs.error[error_msg].count = GMOD_DEBUGGER.logs.error[error_msg].count + 1
+            GMOD_DEBUGGER.logs.error[log.msg].count = GMOD_DEBUGGER.logs.error[log.msg].count + log.data.count
         end
     end
 end)
