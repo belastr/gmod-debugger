d9647c8218c7545bda896ca20bf40de5b08e7b4f
Sooth <128935065+Sooth4@users.noreply.github.com>
2025-04-21 23:31:04 +0200

    feat: collect client errors and send bundled after 5 seconds of no errors

--- lua/gmod-debugger/modules/error/client.lua
+++ lua/gmod-debugger/modules/error/client.lua
@@ -1,2 +1,21 @@
-hook.Add("OnLuaError", "gmod-debugger:error", function(error, _, stack, addon)
+local tmpLogs = {}
+
+timer.Create("gmod-debugger:error", 5, 0, function()
+    for error_msg, d in pairs(tmpLogs) do
+        GMOD_DEBUGGER:SendLog("error", {msg = error_msg, data = d})
+        tmpLogs[error_msg] = nil
+    end
+    timer.Stop("gmod-debugger:error")
+end)
+timer.Stop("gmod-debugger:error")
+
+hook.Add("OnLuaError", "gmod-debugger:error", function(error_msg, _, error_stack)
+    if GMOD_DEBUGGER.config.error.client then
+        if !tmpLogs[error_msg] then
+            tmpLogs[error_msg] = {stack = GMOD_DEBUGGER.config.error.stack && error_stack || false, count = 1}
+        else
+            tmpLogs[error_msg].count = tmpLogs[error_msg].count + 1
+        end
+        timer.Start("gmod-debugger:error")
+    end
 end)
