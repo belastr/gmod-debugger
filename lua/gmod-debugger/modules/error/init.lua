001f79c50f27f28f584b5b5886c7b5fecd783765
Sooth <128935065+Sooth4@users.noreply.github.com>
2025-04-22 20:49:46 +0200

    refactor: initialise module with the new function

--- lua/gmod-debugger/modules/error/init.lua
+++ lua/gmod-debugger/modules/error/init.lua
@@ -1,18 +1,14 @@
-GMOD_DEBUGGER.logs.error = {}
-
-if !GMOD_DEBUGGER.config.error then
-    GMOD_DEBUGGER.config.error = {}
-    GMOD_DEBUGGER.config.error.client = true
-    GMOD_DEBUGGER.config.error.clients = {}
-    GMOD_DEBUGGER.config.error.server = true
-    GMOD_DEBUGGER.config.error.stack = true
-    GMOD_DEBUGGER.config.error.post = false
-end
-
 if CLIENT then
-    print("including client")
     include("gmod-debugger/modules/error/client.lua")
 elseif SERVER then
+    GMOD_DEBUGGER:InitModule("error", {
+        client = {"bool", false},
+        clients = {"plyTbl", {}},
+        server = {"bool", true},
+        stack = {"bool", true},
+        post = {"bool", true}
+    })
+
     AddCSLuaFile("gmod-debugger/modules/error/client.lua")
     include("gmod-debugger/modules/error/server.lua")
 end
