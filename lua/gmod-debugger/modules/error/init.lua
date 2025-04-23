70d58b67665710ca3f27707fc35a0bd785161b73
Sooth <128935065+Sooth4@users.noreply.github.com>
2025-04-23 18:13:58 +0200

    refactor: capitalize type to match standard derma names

--- lua/gmod-debugger/modules/error/init.lua
+++ lua/gmod-debugger/modules/error/init.lua
@@ -2,11 +2,11 @@ if CLIENT then
     include("gmod-debugger/modules/error/client.lua")
 elseif SERVER then
     GMOD_DEBUGGER:InitModule("error", {
-        client = {"bool", false},
-        clients = {"plyTbl", {}},
-        server = {"bool", true},
-        stack = {"bool", true},
-        post = {"bool", true}
+        client = {"Bool", false},
+        //clients = {"PlyTbl", {}},
+        server = {"Bool", true},
+        stack = {"Bool", true},
+        post = {"Bool", true}
     })
 
     AddCSLuaFile("gmod-debugger/modules/error/client.lua")
