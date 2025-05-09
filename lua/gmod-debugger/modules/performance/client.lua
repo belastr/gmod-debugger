28e973d143bcb54700a1ea41e6e50fb672c13b9c
Sooth <128935065+Sooth4@users.noreply.github.com>
2025-05-09 20:18:08 +0200

    feat: add log file print

--- lua/gmod-debugger/modules/performance/client.lua
+++ lua/gmod-debugger/modules/performance/client.lua
@@ -25,6 +25,53 @@ local function logs()
     info:SetText("- means disabled")
     info:SizeToContentsX()
 
+    local logFile = vgui.Create("DButton", labels)
+    logFile:Dock(RIGHT)
+    logFile:SetFont("GModDebuggerFontBold")
+    logFile:SetText("Print to Log File (/data/gmod-debugger/logs/performance)")
+    logFile:SizeToContentsX()
+    logFile.DoClick = function(s)
+        GMOD_DEBUGGER:CreateLogFileFolders("performance")
+        local content = os.date("performance log file generated on %m/%d at %I:%M%p\n\n", os.time())
+        for _, l in ipairs(GMOD_DEBUGGER.logs.performance) do
+            content = content .. os.date("[%m/%d %H:%M:%S] ", l.time)
+
+            content = content .. "clients average fps: "
+            if l.client && l.client != 0 then
+                content = content .. l.client
+            else
+                content = content .. "disabled"
+            end
+
+            content = content .. " - server tickrate: "
+            if l.server && l.server != 0 then
+                content = content .. l.server
+            else
+                content = content .. "disabled"
+            end
+
+            content = content .. "\n"
+        end
+        local suc = file.Write(os.date("gmod-debugger/logs/performance/l_%Y-%m-%d_%H-%M-%S.txt", os.time()), content)
+        if suc then
+            s:SetText("Log File generated")
+            s:SetTextColor(Color(0, 255, 130))
+        else
+            s:SetText("[ERROR] something went wrong")
+            s:SetTextColor(Color(255, 130, 0))
+        end
+        s:SetMouseInputEnabled(false)
+        s:SizeToContentsX()
+    end
+    logFile.Paint = function(s)
+        if !s:IsMouseInputEnabled() then return end
+        if s:IsHovered() then
+            s:SetTextColor(Color(0, 130, 255))
+        else
+            s:SetTextColor(Color(51, 51, 51))
+        end
+    end
+
     for i, l in ipairs(GMOD_DEBUGGER.logs.performance) do
         local log = vgui.Create("DebuggerPerformanceLog", page)
         log:SetData(l)
